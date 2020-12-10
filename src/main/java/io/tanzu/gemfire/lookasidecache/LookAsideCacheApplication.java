68342ebcbb15a8c700fd50ff9a16dc3c8e16ef41
John Martin <48291409+jomartin-999@users.noreply.github.com>
2020-12-10 12:15:57 -0500

    Updated the app with a different cluster name

--- src/main/java/io/tanzu/gemfire/lookasidecache/LookAsideCacheApplication.java
+++ src/main/java/io/tanzu/gemfire/lookasidecache/LookAsideCacheApplication.java
@@ -19,6 +19,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LookAsideCacheApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(LookAsideCacheApplication.class, args);
     }
